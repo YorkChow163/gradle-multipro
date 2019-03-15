eae7d21429a0eda7effb84ce10325b24dc2253d9
yorkchow163 <2171677178@qq.com>
2019-03-15 17:47:59 +0800

    添加依赖成功

---
+++ springsecurity-demo/src/main/java/com/zhouyu/securitydemo/SpringSecurityDemoApplication.java
@@ -0,0 +1,16 @@
+package com.zhouyu.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: zhouyu
+ * @Date: 2019/3/15 17:40
+ */
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+     public static void main(String[] args) {
+             SpringApplication.run(String.class, args);
+         }
+}
