fcf18b444fbdd12348de5df705ff7a892bd1a734
YorkChow163 <2171677178@qq.com>
2019-04-04 11:21:20 +0800

    静态的资源无法发现

--- springsecurity-demo/src/main/java/com/zhouyu/securitydemo/controller/UserLoginController.java
+++ springsecurity-demo/src/main/java/com/zhouyu/securitydemo/controller/UserLoginController.java
@@ -1,32 +1,41 @@
 package com.zhouyu.securitydemo.controller;
 
-import com.zhouyu.securitydemo.entity.MyUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Description:用户登录退出业务
  * @Date:2019/3/25 15:30
  * @Author:zhouyu
  */
-@RestController
+@Controller
 public class UserLoginController {
    private static Logger logger = LoggerFactory.getLogger(UserLoginController.class);
 
     @RequestMapping("/login")
-    public String userLogin(@RequestBody MyUser user){
-        logger.info("登录啦,user:{}",user.toString());
+    public String goLoginPage(){
+        logger.info("去登陆页面!!!");
         return "login";
+    }
+
+    /**
+     * 获取验证码
+     * @return
+     */
+    @RequestMapping(name = "/captcha",method = RequestMethod.GET)
+    public String getCaptcha(@RequestParam(value = "ID") String ID){
+        return "";
     }
 
     @RequestMapping("/index")
     @ResponseBody
     public String gotoIndexPage(){
-        logger.info("去首页");
+        logger.info("去首页!!!");
         return "zhouyu";
     }
 }
