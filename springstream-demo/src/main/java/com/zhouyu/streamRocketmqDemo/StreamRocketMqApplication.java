c02569c072e2c3d037ca206bbe0ac5cf7c4b5707
YorkChow163 <2171677178@qq.comm>
2019-04-14 22:13:40 +0800

    springstream-rocketmq依赖

---
+++ springstream-demo/src/main/java/com/zhouyu/streamRocketmqDemo/StreamRocketMqApplication.java
@@ -0,0 +1,17 @@
+package com.zhouyu.streamRocketmqDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author:zhouyu
+ * @Time:2019/4/14 22:11
+ */
+@SpringBootApplication
+public class StreamRocketMqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRocketMqApplication.class, args);
+    }
+
+}
