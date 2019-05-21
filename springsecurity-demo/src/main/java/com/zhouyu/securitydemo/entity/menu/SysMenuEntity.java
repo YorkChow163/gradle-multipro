a708de9e017c615da56756b3e74a994b1cd9f84d
YorkChow163 <2171677178@qq.com>
2019-05-21 15:55:25 +0800

    菜单entity

--- springsecurity-demo/src/main/java/com/zhouyu/securitydemo/entity/menu/SysMenuEntity.java
+++ springsecurity-demo/src/main/java/com/zhouyu/securitydemo/entity/menu/SysMenuEntity.java
@@ -9,10 +9,14 @@
 package com.zhouyu.securitydemo.entity.menu;
 
 import com.zhouyu.securitydemo.entity.BaseEntity;
-import lombok.Data;
+
+import org.springframework.data.annotation.Transient;
+
+import java.util.List;
 
 import javax.persistence.Table;
-import java.util.List;
+
+import lombok.Data;
 
 /**
  * 菜单管理
@@ -23,50 +27,52 @@ import java.util.List;
 @Table(name = "sys_menu")
 public class SysMenuEntity extends BaseEntity {
 
-    /**
-     * 父菜单ID，一级菜单为0
-     */
-    private Long parentId;
+  /**
+   * 父菜单ID，一级菜单为0
+   */
+  private Long parentId;
 
-    /**
-     * 父菜单名称
-     */
-    private String parentName;
+  /**
+   * 父菜单名称
+   */
+  @Transient
+  private String parentName;
 
-    /**
-     * 菜单名称
-     */
-    private String name;
+  /**
+   * 菜单名称
+   */
+  private String name;
 
-    /**
-     * 菜单URL
-     */
-    private String url;
+  /**
+   * 菜单URL
+   */
+  private String url;
 
-    /**
-     * 授权(多个用逗号分隔，如：user:list,user:create)
-     */
-    private String perms;
+  /**
+   * 授权(多个用逗号分隔，如：user:list,user:create)
+   */
+  private String perms;
 
-    /**
-     * 类型     0：目录   1：菜单   2：按钮
-     */
-    private Integer type;
+  /**
+   * 类型     0：目录   1：菜单   2：按钮
+   */
+  private Integer type;
 
-    /**
-     * 菜单图标
-     */
-    private String icon;
+  /**
+   * 菜单图标
+   */
+  private String icon;
 
-    /**
-     * 排序
-     */
-    private Integer orderNum;
+  /**
+   * 排序
+   */
+  private Integer orderNum;
 
-    /**
-     * ztree属性
-     */
-    private Boolean open;
-
-    private List<?> list;
+  /**
+   * ztree属性
+   */
+  @Transient
+  private Boolean open;
+  @Transient
+  private List<?> list;
 }
